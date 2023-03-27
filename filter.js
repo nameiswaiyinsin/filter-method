271d31f5c595fca43231e7d9db81e96fff41f3e8
nameiswaiyinsin <waiyinsin@gmail.com>
2023-03-27 10:38:43 +0000

    Added filter() method examples and challenges

--- filter.js
+++ filter.js
@@ -21,6 +21,12 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === 'Paul')[0];
+console.log(paul);
+
 
 // Complex Filtering
 const students = [
@@ -55,3 +61,57 @@ const students = [
     ]
   },
 ];
+
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidate = students.filter(hasStrongSkills);
+const names = candidate.map(students => students.name);
+console.log(names);
+
+
+
+
+//Example 2: Challenge: Find people who have scored 90 and above in maths in a single line arrow code, using filter() method
+let studentsA = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+//write code here:
+
+const topMaths = studentsA.filter(student => student.results.maths >= 90);
+console.log(topMaths);
+
+
+//answers: 
+
+// [ { name: 'John',
+//   subjects: [ 'maths', 'english', 'cad' ],
+//   teacher: { maths: 'Harry', english: 'Joan', cad: 'Paul' },
+//   results: { maths: 90, english: 75, cad: 87 } },
+// { name: 'Adam',
+//   subjects: [ 'science', 'maths', 'art' ],
+//   teacher: { science: 'Iris', maths: 'Harry', art: 'Simon' },
+//   results: { science: 84, maths: 97, art: 95 } } ]
